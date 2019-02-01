c5c5fd166e4de6b5be48ad6ca11a4eb4e4a95057
SaffronSushi <45082028+SaffronSushi@users.noreply.github.com>
2019-02-01 12:54:09 -0700

    More initial files

---
+++ star.py
@@ -0,0 +1,56 @@
+import pygame, random
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, game, life):
+        super(Star, self).__init__()
+        self.screen = game.screen
+        self.life = life
+
+        self.color = ((255, 255, 255))
+        self.active_clr = ((200, 200, 255))
+        self.size = int(self.life)
+        self.radius = int(self.size / 2)
+        self.image = pygame.Surface((self.size, self.size))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        
+        self.rect.centerx = random.randint(self.radius,
+                        self.screen.get_width() - self.radius)
+        self.rect.centery = random.randint(self.radius,
+                        self.screen.get_height() - self.radius)
+
+        self.clicked = False
+        self.active = False
+
+    def update(self, delta_time, cursor):
+        self.dt = delta_time
+
+        self.set_size()
+        self.check_events(cursor)
+        if self.active:
+            self.image.fill(self.active_clr)
+        else:
+            self.image.fill(self.color)
+
+    def set_size(self):
+        old_center = self.rect.center
+        self.life -= 4 * self.dt
+        if self.life < 0:
+            self.life = 0
+            
+        self.size = int(self.life / 10)
+        self.image = pygame.Surface((self.size, self.size))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.center = old_center
+
+    def check_events(self, cursor):
+        if pygame.mouse.get_pressed() == (1, 0, 0):
+            if pygame.sprite.collide_rect(cursor, self):
+                self.clicked = True
+
+        if self.clicked == True:
+            if pygame.mouse.get_pressed() == (0, 0, 0):
+                self.clicked = False
+                if pygame.sprite.collide_rect(cursor, self):
+                    self.active = True
